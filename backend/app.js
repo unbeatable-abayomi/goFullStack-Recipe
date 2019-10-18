203700b6f9f994ef512e7f2912c5efa7a1b31d80
abayomi igwubor <igwubor@outlook.com>
2019-10-18 13:17:54 +0100

    installedMonogoDbandImplementedIt

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,23 @@
+// MONOGODB PASSWORD == ouQXHoCgq7dFuCAZ
+
+// MONOGODB CONNECTION   
+// mongodb+srv://abayomi:<password>@cluster0-wnkzn.mongodb.net/test?retryWrites=true&w=majority
+
 const express = require('express');
-
-const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
 
+
+mongoose.connect('mongodb+srv://abayomi:ouQXHoCgq7dFuCAZ@cluster0-wnkzn.mongodb.net/test?retryWrites=true&w=majority')
+.then(()=>{
+    console.log('Successfully to monogDB Atals')
+})
+.catch((error)=>{
+    console.log("unable to connect to MonogoDB")
+    console.error(error)
+
+})
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
