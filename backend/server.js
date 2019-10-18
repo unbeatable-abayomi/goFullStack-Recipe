908bd804552992ba1f6cd8b832186a4167eafb27
abayomi igwubor <igwubor@outlook.com>
2019-10-18 12:07:16 +0100

    installedExpress&Nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-console.log('server dot js is running')
+
 const http = require('http');
-const server = http.createServer((req, res)=>{
-    res.end('This is my server respone now now');
-});
+const app = require('./app');
+app.set('port', process.env.PORT || 3000);
+const server = http.createServer(app);
 
 server.listen(process.env.PORT || 3000)
